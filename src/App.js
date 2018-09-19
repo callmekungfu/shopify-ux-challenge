5c30201711cb7b90428b8b25e24aa6710b164f11
Yong Lin Wang <callmekungfu@users.noreply.github.com>
2018-09-19 16:05:24 -0400

    Compeleted the login form updated the font face

--- src/App.js
+++ src/App.js
@@ -7,11 +7,18 @@ class App extends Component {
     return (
       <div className="App">
         <div className="login-container container">
-          <form>
-            <input type="text" placeholder="username" className="text-purple"></input>
-            <input type="password" placeholder="password" className="text-purple"></input>
-            <input type="submit" value="Login"/>
-          </form>
+            <form>
+              <div className="login-inner">
+                <input type="text" placeholder="username" className="text-purple border-bottom-purple shadowed input"></input>
+                <input type="password" placeholder="password" className="text-purple shadowed input"></input>
+                <input type="submit" value="Login" className="bg-purple shadowed input"/>
+              </div>
+            </form>
+            <div className="rmb-me-input">
+              <input type="checkbox" name="rememberMe" className="checkbox"/><span class="checkmark"></span>
+              <label className="text-purple">remember me</label>
+            </div>
+            
         </div>
         <div className="static-container container"></div>
       </div>
